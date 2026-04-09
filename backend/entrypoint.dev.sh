d91b3ef44f3f7f06da6b7d796ddfe0f33d9c8711
Фунтиков Михаил Николаевич <fmn@ttit.local>
2026-04-09 12:03:44 +0700

    dev dockerfile update

--- backend/entrypoint.dev.sh
+++ backend/entrypoint.dev.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-composer install --prefer-dist --optimize-autoloader --no-scripts --no-interaction
-
 chmod -R 777 storage/ bootstrap/cache/
 
 php artisan migrate --seed --force
